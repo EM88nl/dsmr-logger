86d0aeb9b85b74e3de1a973657dfc5d10ded568b
Erik Michiels <e.michiels1988-github@yahoo.com>
2020-12-23 18:58:56 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿namespace dsmr_logger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ SerialReader.cs
@@ -0,0 +1,19 @@
+﻿using System.IO.Ports;
+
+public class SerialReader
+{
+    public SerialReader()
+    {
+        // Create and open the serial port
+        _serialPort = new SerialPort("/dev/ttyUSB0", 115200, Parity.None, 8, StopBits.One);
+        _serialPort.Open();
+    }
+
+    ~SerialReader()
+    {
+        _serialPort.Close();
+    }
+    static SerialPort _serialPort;
+
+
+}
